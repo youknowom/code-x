2e389ea05dcfcd177b9dedf78f6fa9e8715391f4
Omkaar <omdb2004@gmail.com>
2025-12-13 22:26:31 +0530

    Remove unused Header and Hero components from Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
       {/* Header */}
-
+      <Header />
       {/* Hero Section */}
+      <Hero />
     </div>
   );
 }
