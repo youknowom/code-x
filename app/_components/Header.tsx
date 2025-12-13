128fa77afbcc81f189f59f3256250a3c0a2073c1
Omkaar <omdb2004@gmail.com>
2025-12-13 22:35:19 +0530

    Refactor Header component structure to group logo and title within a flex container

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -4,8 +4,10 @@ import React from "react";
 function Header() {
   return (
     <div>
-      <Image src={"/logo.png"} alt="logo" width={40} height={40} />{" "}
-      <h2 className="font-bold text-2xl font-game">CodeTree</h2>
+      <div className="flex gap-2 items-center">
+        <Image src={"/logo.png"} alt="logo" width={40} height={40} />
+        <h2 className="font-bold text-2xl font-game">CodeTree</h2>
+      </div>
     </div>
   );
 }
