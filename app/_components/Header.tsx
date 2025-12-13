4dc5676e47aa1f2ee5a77d92606f03bcec1f686c
Omkaar <omdb2004@gmail.com>
2025-12-13 22:52:48 +0530

    Refactor Header component layout to use justify-between for improved alignment

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,17 +1,17 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 function Header() {
   return (
-    <div className="p-4 max-w-7xl flex justify-center items-center w-full">
+    <div className="p-4 max-w-7xl flex justify-between items-center w-full">
       <div className="flex gap-2 items-center cursor-pointer">
-        <Image src={"/logo.png"} alt="logo" width={40} height={40} />
+        <Image src="/logo.png" alt="logo" width={40} height={40} />
         <h2 className="font-bold text-2xl font-game">CodeTree</h2>
       </div>
-      {/* Navbar */}
 
-      {/* Signuo Button */}
-      <Button className="font-game text-2xl" variant={"pixel"}>
+      <Button className="font-game text-2xl" variant="pixel">
         Signup
       </Button>
     </div>
