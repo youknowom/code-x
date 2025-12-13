d5f36389221aa892efc108b423d3141fc989cba6
Omkaar <omdb2004@gmail.com>
2025-12-13 22:36:52 +0530

    Enhance layout of Header and Home components with appropriate styling classes

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
+    <div className="p-4 max-w-7xl">
       <div className="flex gap-2 items-center">
         <Image src={"/logo.png"} alt="logo" width={40} height={40} />
         <h2 className="font-bold text-2xl font-game">CodeTree</h2>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       {/* Header */}
       <Header />
       {/* Hero Section */}
