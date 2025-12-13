a7bb30f183d7995e19b068cd96f7f94fc5fa5877
Omkaar <omdb2004@gmail.com>
2025-12-13 22:30:08 +0530

    Update Header component to display logo image

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Image src={"/logo.png"} alt="logo" width={40} height={40} />
+    </div>
+  );
 }
 
 export default Header;
