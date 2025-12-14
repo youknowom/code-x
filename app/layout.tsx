802bcccd9bc427bae1e9f433a699814b3957996a
Omkaar <omdb2004@gmail.com>
2025-12-14 13:00:25 +0530

    Implement Clerk authentication routes and middleware for user sign-in and sign-up

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pixelify_Sans, Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "./Provider";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
