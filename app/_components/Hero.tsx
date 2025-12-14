ce397633b3211ade60acc836f09c7bcb47bf44cd
Omkaar <omdb2004@gmail.com>
2025-12-14 12:27:32 +0530

    Implement Hero component with background image for enhanced visual appeal

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="w-full relative h-screen overflow-hidden">
+      <Image
+        src={"/bg.gif"}
+        alt="hero"
+        width={1000}
+        height={1000}
+        className="w-full h-full object-cover absolute inset-0"
+      ></Image>
+    </div>
+  );
 }
 
 export default Hero;
