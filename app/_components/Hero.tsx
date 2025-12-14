49fcb3034461e28926eec9e7bb628fd31c593ea8
Omkaar <omdb2004@gmail.com>
2025-12-14 12:43:21 +0530

    Add Hero component content and button for user engagement

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 
@@ -14,13 +15,18 @@ function Hero() {
 
       <div className="absolute w-full flex flex-col items-center mt-24">
         <h2 className="font-bold text-7xl font-game">Start</h2>
-
         <h2
           className=" font-game text-[48px] leading-[1] font-bold text-8xl font-game text-yellow-400"
           style={{ textShadow: "2px 2px 0 #000,-2px -2px 0 #000" }}
         >
           Coding Adventure
         </h2>
+        <h2 className="mt-5 font-game text-3xl">
+          beginner-friendly coding courses and projects
+        </h2>
+        <Button className="font-game text-3xl p-6" variant={"pixel"}>
+          GET STARTED
+        </Button>
       </div>
     </div>
   );
