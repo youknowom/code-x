4985a2d4d72df0fbbd31e60687c33bd17ea3a09f
Omkaar <omdb2004@gmail.com>
2025-12-16 14:43:49 +0530

    Add startup and tree images to the public directory
    - Added start-up.png (3,019 bytes) to enhance the user interface.
    - Added tree.png (4,764 bytes) for improved visual representation.

--- app/(routes)/dashboard/_components/WelcomeBanner.tsx
+++ app/(routes)/dashboard/_components/WelcomeBanner.tsx
@@ -1,7 +1,15 @@
+import { User } from "@clerk/nextjs/server";
+import Image from "next/image";
 import React from "react";
 
 function WelcomeBanner() {
-  return <div>WelcomeBanner</div>;
+  return (
+    <div>
+      <Image src={"/machine.webp"} alt="robo" width={120} height={120}></Image>
+
+      <h2>Welcome Back, {User?.fullname}</h2>
+    </div>
+  );
 }
 
 export default WelcomeBanner;
