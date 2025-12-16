3ecabfb022ae815a1e75a7dc5303ff784f0ec652
Omkaar <omdb2004@gmail.com>
2025-12-16 16:17:27 +0530

    chore: update WelcomeBanner component to use useUser hook and enhance layout

--- app/(routes)/dashboard/_components/WelcomeBanner.tsx
+++ app/(routes)/dashboard/_components/WelcomeBanner.tsx
@@ -1,13 +1,22 @@
-import { User } from "@clerk/nextjs/server";
+"use client";
+
 import Image from "next/image";
 import React from "react";
+import { useUser } from "@clerk/nextjs";
 
 function WelcomeBanner() {
-  return (
-    <div>
-      <Image src={"/machine.webp"} alt="robo" width={120} height={120}></Image>
+  const { user } = useUser();
 
-      <h2>Welcome Back, {User?.fullname}</h2>
+  return (
+    <div className="flex gap-3 items-center">
+      <Image src="/machine.webp" alt="robo" width={120} height={120} />
+
+      <h2 className="font-game text-2xl p-2 border bg-zinc-800 rounded-lg rounded-bl-none">
+        Welcome Back,{" "}
+        <span className="text-yellow-300">{user?.fullName ?? "Learner"}</span> !
+        <br />
+        Start Learning Something New 🚀
+      </h2>
     </div>
   );
 }
