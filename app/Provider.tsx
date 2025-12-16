d45c6a7987a8006e82be1020b833945c78018ebe
Omkaar <omdb2004@gmail.com>
2025-12-16 13:02:29 +0530

    chore: refactor user creation logic in Provider component for clarity and consistency

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,22 +1,28 @@
+"use client";
+
 import React, { useEffect } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useUser } from "@clerk/nextjs";
 import axios from "axios";
+
 function Provider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
   const { user } = useUser();
+
   useEffect(() => {
-    user && CreateNewUser();
-  });
-  const CreateNewUser = async () => {
+    if (user) {
+      createNewUser();
+    }
+  }, [user]);
+
+  const createNewUser = async () => {
     const result = await axios.post("/api/user", {});
     console.log(result);
   };
 
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
-//
 
 export default Provider;
