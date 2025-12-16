c6f6df298303da32d264436eebae185a1065af01
Omkaar <omdb2004@gmail.com>
2025-12-16 10:31:42 +0530

    chore: add axios dependency and implement user creation logic in Provider component

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { useUser } from "@clerk/nextjs";
-
+import axios from "axios";
 function Provider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
   const { user } = useUser();
-
-  const CreateNewUser = () => {};
+  useEffect(() => {
+    user && CreateNewUser();
+  });
+  const CreateNewUser = async () => {
+    const result = await axios.post("/api/user", {});
+    console.log(result);
+  };
 
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
