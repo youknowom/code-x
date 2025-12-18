ed4578a705ac415f539e7f85550562e8468565a8
Omkaar <omdb2004@gmail.com>
2025-12-18 18:29:32 +0530

    feat: implement InviteFriend component with image and invitation message

--- app/(routes)/dashboard/_components/InviteFriend.tsx
+++ app/(routes)/dashboard/_components/InviteFriend.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
 import React from "react";
 
 function InviteFriend() {
-  return <div>InviteFriend</div>;
+  return (
+    <div>
+      <Image src={"/mail.png"} alt="mail" width={80} height={80} />
+      <h2 className="font-3xl font-game">Invite Frined</h2>{" "}
+      <p>
+        Having Fun? Share the love with a friend ! Enter an amil and we will
+        send them a personal invite
+      </p>
+    </div>
+  );
 }
 
 export default InviteFriend;
