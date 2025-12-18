6481187d4bd7323543a7220a4a48341c71dfb654
Omkaar <omdb2004@gmail.com>
2025-12-18 18:32:23 +0530

    feat: enhance InviteFriend component layout and functionality

--- app/(routes)/dashboard/_components/InviteFriend.tsx
+++ app/(routes)/dashboard/_components/InviteFriend.tsx
@@ -1,15 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
-import React from "react";
 
 function InviteFriend() {
   return (
-    <div>
+    <div className="flex flex-col items-center mt-8 p-4 border rounded-xl">
       <Image src={"/mail.png"} alt="mail" width={80} height={80} />
       <h2 className="font-3xl font-game">Invite Frined</h2>{" "}
       <p>
         Having Fun? Share the love with a friend ! Enter an amil and we will
         send them a personal invite
       </p>
+      <div>
+        <Input placeholder="Enter Invite Email" />
+        <Button variant={"pixel"} className="font-game">
+          Invite
+        </Button>
+      </div>
     </div>
   );
 }
