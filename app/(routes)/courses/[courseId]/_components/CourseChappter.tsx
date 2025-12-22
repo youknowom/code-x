5abb39bcfec5734c29f9e3b720f94318b0192d35
Omkaar <omdb2004@gmail.com>
2025-12-22 13:20:03 +0530

    feat: implement chapter rendering in CourseChappter component with accordion layout

--- app/(routes)/courses/[courseId]/_components/CourseChappter.tsx
+++ app/(routes)/courses/[courseId]/_components/CourseChappter.tsx
@@ -1,13 +1,33 @@
 import React from "react";
 import { Course } from "../../_components/CourseList";
-
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 type Props = {
   loading: boolean;
   courseDetail: Course | undefined;
 };
 
 function CourseChappter({ loading, courseDetail }: Props) {
-  return <div>CourseChappter</div>;
+  return (
+    <div>
+      <div>
+        {courseDetail?.chapters?.map((chapter, index) => (
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Is it accessible?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CourseChappter;
