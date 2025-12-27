696c46a85756e850b578deacb9afdf012284e0c2
Omkaar <omdb2004@gmail.com>
2025-12-27 12:45:40 +0530

    refactor: remove commented-out code and clean up GET function structure

--- app/api/course/route.ts
+++ app/api/course/route.ts
@@ -1,9 +1,3 @@
-// import CourseChappter from "@/app/(routes)/courses/[courseId]/_components/CourseChappter";
-// import { db } from "@/config/db";
-// import { courseChaptersTable, coursesTable } from "@/config/schema";
-// import { eq } from "drizzle-orm";
-// import { NextRequest, NextResponse } from "next/server";
-
 import { db } from "@/config/db";
 import {
   courseChaptersTable,
@@ -14,24 +8,6 @@ import {
 import { currentUser } from "@clerk/nextjs/server";
 import { and, eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
-
-// export async function GET(req: NextRequest) {
-//   const { searchParams } = new URL(req.url);
-//   const courseId = searchParams.get("courseId");
-//   if (courseId) {
-//     const parsedCourseId = parseInt(courseId, 10);
-//     const result = await db
-//       .select()
-//       .from(coursesTable)
-//       .where(eq(coursesTable.courseId, parsedCourseId));
-//     const chapterResult = await db.select().from(courseChaptersTable).where(eq);
-//     return NextResponse.json(result[0]);
-//   } else {
-//     const result = await db.select().from(coursesTable);
-
-//     return NextResponse.json(result);
-//   }
-// }
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
