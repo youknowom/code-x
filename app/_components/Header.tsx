25897f897ca2dd81fa5ad0cac313a7601cd8782b
Omkaar <omdb2004@gmail.com>
2025-12-30 22:28:48 +0530

    feat: fetch courses from API and update navigation menu to display course titles and descriptions

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -13,69 +13,81 @@ import {
 } from "@/components/ui/navigation-menu";
 import { UserButton, useUser } from "@clerk/nextjs";
 import { useParams, usePathname } from "next/navigation";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Course } from "../(routes)/courses/_components/CourseList";
 
-const courses = [
-  {
-    id: 1,
-    name: "HTML",
-    desc: "Learn the fundamentals of HTML and build the structure of modern web pages.",
-    path: "/course/1/detail",
-  },
-  {
-    id: 2,
-    name: "CSS",
-    desc: "Master CSS to style and design responsive, visually appealing web layouts.",
-    path: "/course/2/detail",
-  },
-  {
-    id: 3,
-    name: "React",
-    desc: "Build dynamic and interactive web applications using React.",
-    path: "/course/3/detail",
-  },
-  {
-    id: 4,
-    name: "React Advanced",
-    desc: "Deep dive into advanced React concepts.",
-    path: "/course/4/detail",
-  },
-  {
-    id: 5,
-    name: "Python",
-    desc: "Learn Python from basics to real-world applications.",
-    path: "/course/5/detail",
-  },
-  {
-    id: 6,
-    name: "Python Advanced",
-    desc: "Advanced Python concepts like OOP and APIs.",
-    path: "/course/6/detail",
-  },
-  {
-    id: 7,
-    name: "Generative AI",
-    desc: "Explore prompt engineering, LLMs, and GenAI apps.",
-    path: "/course/7/detail",
-  },
-  {
-    id: 8,
-    name: "Machine Learning",
-    desc: "Learn ML algorithms and deployment.",
-    path: "/course/8/detail",
-  },
-  {
-    id: 9,
-    name: "JavaScript",
-    desc: "Master modern JavaScript and async programming.",
-    path: "/course/9/detail",
-  },
-];
+// const courses = [
+//   {
+//     id: 1,
+//     name: "HTML",
+//     desc: "Learn the fundamentals of HTML and build the structure of modern web pages.",
+//     path: "/course/1/detail",
+//   },
+//   {
+//     id: 2,
+//     name: "CSS",
+//     desc: "Master CSS to style and design responsive, visually appealing web layouts.",
+//     path: "/course/2/detail",
+//   },
+//   {
+//     id: 3,
+//     name: "React",
+//     desc: "Build dynamic and interactive web applications using React.",
+//     path: "/course/3/detail",
+//   },
+//   {
+//     id: 4,
+//     name: "React Advanced",
+//     desc: "Deep dive into advanced React concepts.",
+//     path: "/course/4/detail",
+//   },
+//   {
+//     id: 5,
+//     name: "Python",
+//     desc: "Learn Python from basics to real-world applications.",
+//     path: "/course/5/detail",
+//   },
+//   {
+//     id: 6,
+//     name: "Python Advanced",
+//     desc: "Advanced Python concepts like OOP and APIs.",
+//     path: "/course/6/detail",
+//   },
+//   {
+//     id: 7,
+//     name: "Generative AI",
+//     desc: "Explore prompt engineering, LLMs, and GenAI apps.",
+//     path: "/course/7/detail",
+//   },
+//   {
+//     id: 8,
+//     name: "Machine Learning",
+//     desc: "Learn ML algorithms and deployment.",
+//     path: "/course/8/detail",
+//   },
+//   {
+//     id: 9,
+//     name: "JavaScript",
+//     desc: "Master modern JavaScript and async programming.",
+//     path: "/course/9/detail",
+//   },
+// ];
 
 export default function Header() {
   const { user } = useUser();
   const path = usePathname();
   const { exerciseslug } = useParams();
+  const [courses, setCourses] = useState<Course[]>([]);
 
+  useEffect(() => {
+    GetCourses();
+  }, []);
+
+  const GetCourses = async () => {
+    const result = await axios.get("/api/course");
+    setCourses(result.data);
+  };
   return (
     <div className="p-4 max-w-7xl mx-auto flex justify-between items-center w-full">
       {/* Logo */}
@@ -85,7 +97,7 @@ export default function Header() {
       </Link>
 
       {/* Navigation */}
-      {!exerciseslug ? (
+      {!exerciseslug && courses ? (
         <NavigationMenu>
           <NavigationMenuList className="gap-8">
             {/* Courses Dropdown */}
@@ -94,14 +106,14 @@ export default function Header() {
               <NavigationMenuContent>
                 <ul className="grid md:grid-cols-2 sm:w-[400px] md:w-[500px] lg:w-[600px] p-4 gap-2">
                   {courses.map((course) => (
-                    <li key={course.id}>
+                    <li key={course.courseId}>
                       <Link
-                        href={course.path}
+                        href={"/courses/" + course.courseId}
                         className="block rounded-md p-3 hover:bg-accent transition"
                       >
-                        <h3 className="font-medium">{course.name}</h3>
+                        <h3 className="font-medium">{course.title}</h3>
                         <p className="text-sm text-muted-foreground">
-                          {course.desc}
+                          {course.description}
                         </p>
                       </Link>
                     </li>
