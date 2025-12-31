0b5daa902d293dd2f612f74d6c22004a395661bc
Omkaar <omdb2004@gmail.com>
2025-12-31 09:50:54 +0530

    feat: enhance Pricing component layout and styling

--- app/(routes)/pricing/page.tsx
+++ app/(routes)/pricing/page.tsx
@@ -3,7 +3,14 @@ import React from "react";
 
 function Pricing() {
   return (
-    <div style={{ maxWidth: "800px", margin: "0 auto", padding: "0 1rem" }}>
+    <div
+      className="mt-28"
+      style={{ maxWidth: "800px", margin: "0 auto", padding: "0 1rem" }}
+    >
+      <h2 className="text-3xl text-center">Pricing</h2>
+      <h2 className="text-xl text-center">
+        Join For Unlimeted access to all feature and courses
+      </h2>
       <PricingTable />
     </div>
   );
